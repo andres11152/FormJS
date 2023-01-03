c4dfc8f8f5eea060267e5bb0b9e3e0f16447e1ff
andresbetancourto <andres11152@gmail.com>
2023-01-03 13:42:28 -0500

    proyecto finalizado2

---
+++ components/readTasks.js
@@ -0,0 +1,26 @@
+// Vamos a leer la información que tenemos en localStorage
+import { createTask } from "./addTask.js";
+import { uniqueDates,orderDates } from "../services/date.js";
+import dateElement from "./dateElement.js";
+
+
+export const displayTasks = () => {
+    
+
+    const list = document.querySelector("[data-list");
+    const tasksList = JSON.parse(localStorage.getItem("tasks")) || []; // en caso de q venga null , tendrá por defecto un arreglo vacio
+    const dates = uniqueDates(tasksList);
+    orderDates(dates);
+    
+    dates.forEach(date => {
+        const dateMoment = moment(date, "DD/MM/YYYY");
+        list.appendChild(dateElement(date));
+        tasksList.forEach((task) => {
+            const taskDate = moment(task.dateFormat, "DD/MM/YYYY");
+            const diff = dateMoment.diff(taskDate);
+            if (diff == 0){
+            list.appendChild(createTask(task));
+            }
+        });
+    }); 
+};
